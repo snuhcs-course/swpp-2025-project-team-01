defb3c5dc7ef62e4c2afb84cbfd06a3b7dc3f9e8
Hahndeul Kim <del2090@snu.ac.kr>
2025-11-27 14:46:39 +0900

    fix: Unify menu shapes

--- frontend/lib/features/home/custom_drawer.dart
+++ frontend/lib/features/home/custom_drawer.dart
@@ -9,9 +9,18 @@ import 'package:re_view/core/localization/app_localizations.dart';
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
+  static const ShapeBorder drawerShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.horizontal(
+      right: Radius.circular(16),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Drawer(child: _DrawerContent());
+    return Drawer(
+      shape: drawerShape,
+      child: _DrawerContent()
+    );
   }
 
   /// 모션 줄이기 모드용 다이얼로그 표시
@@ -24,6 +33,7 @@ class CustomDrawer extends StatelessWidget {
         child: Material(
           color: Theme.of(context).canvasColor,
           elevation: 16,
+          shape: drawerShape,
           child: SizedBox(
             width: MediaQuery.of(context).size.width * 0.75,
             height: double.infinity,
